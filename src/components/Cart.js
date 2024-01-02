dabefab6e550fc565b5ed4f1cd9d10629116cdf7
Madhuraaa06 <madhumanjrekar06@gmail.com>
2024-01-02 15:17:38 +0530

    cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,22 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import '../styling/style.css';
 import DataContext from '../context/data/dataContext';
 
 function Cart() {
   const { cart, deleteitem, setCart } = useContext(DataContext);
+  const [quantity, setQuantity] = useState(1);
+
+
+  const handleQuantityChange = (productId, change) => {
+    setCart((prevCart) =>
+      prevCart.map((product) =>
+        product._id === productId
+          ? { ...product, quantity: Math.max(1, (product.quantity || 1) + change) }
+          : product
+      )
+    );
+  };
+
 
   // Load cart data from local storage on component mount
   useEffect(() => {
@@ -18,10 +31,18 @@ function Cart() {
     localStorage.setItem('cart', JSON.stringify(cart));
   }, [cart]);
 
+  let subtotal = 0;
+  cart.forEach((product)=> {
+    subtotal+= (product.quantity || 1)*(product.price)
+    
+  });
+
+
+
   return (
     <div>
       <div className="cart">
-        <table>
+        <table className='pcart'>
           <thead>
             <tr>
               <th>Product</th>
@@ -36,14 +57,24 @@ function Cart() {
                 <td>
                   <div className="product-info">
                     <img src={`newimages/${product.imgUrl}.png`} alt="" />
-                    <h2>{product.name}</h2>
-                    <p>{product.description}</p>
+                    <div className='product-info1'><h2>{product.name}</h2>
+                      <p>{product.description}</p>
+                    </div>
                   </div>
                 </td>
+
                 <td className="quantity">
-                  <input type="number" value="1" />
+                  <button
+                    type="button"className="mbutton"
+                    onClick={() => handleQuantityChange(product._id, -1)}>-</button>
+                  <input type="number" value={product.quantity || 1}  />
+
+                  <button
+                    type="button"className="pbutton"
+                    onClick={() => handleQuantityChange(product._id, 1)}>+</button>
                 </td>
-                <td className="price">₹{product.price}</td>
+
+                <td className="price">₹{product.quantity * product.price}</td>
                 <td className="remove">
                   <i className="fa-solid fa-trash" onClick={() => deleteitem(product._id)}></i>
                 </td>
@@ -54,18 +85,20 @@ function Cart() {
 
         <table className="totalsum">
           <tbody>
+            <th>ORDER SUMMARY</th>
             <tr>
               <td>Subtotal :</td>
-              <td>123</td>
+              <td>{subtotal}</td>
             </tr>
             <tr>
               <td>Tax :</td>
-              <td>123</td>
+              <td>0 guys enjoyyy</td>
             </tr>
             <tr>
               <td>To pay :</td>
-              <td>123</td>
+              <td>{subtotal}</td>
             </tr>
+            <button className='checkout' type='button'>Checkout </button>
           </tbody>
         </table>
       </div>
